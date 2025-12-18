940477f9beb3fa881e9b37499ab2312b5267a74f
Mddu11 <vodilov_vano@mail.ru>
2025-12-18 18:02:58 +0300

    Update Warrior.js

--- src/js/сharacters/Warrior.js
+++ src/js/сharacters/Warrior.js
@@ -1,6 +1,32 @@
-import Player from './Player.js';
-import Sword from '../weapons/Sword.js';
+import { Player } from './Player.js';
+import { Sword } from '../weapons/Sword.js';
 
-export default class Warrior extends Player {
-  static startWeapon = Sword;
+export class Warrior extends Player {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 120;
+        this.speed = 2;
+        this.description = 'Воин';
+        this.weapon = new Sword();
+    }
+
+    takeDamage(damage) {
+        const lowHealth = this.life < 60;
+        const hasMagic = this.magic > 0;
+        const lucky = this.getLuck() > 0.8;
+
+        if (lowHealth && hasMagic && lucky) {
+            const absorbed = Math.min(this.magic, damage);
+            this.magic -= absorbed;
+
+            damage -= absorbed;
+        }
+
+        if (damage > 0) {
+            this.life -= damage;
+            if (this.life < 0) {
+                this.life = 0;
+            }
+        }
+    }
 }
