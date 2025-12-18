b63b9093c0f058c467d06a154e61da727554208f
Mddu11 <vodilov_vano@mail.ru>
2025-12-18 17:57:06 +0300

    Update Dwart.js

--- src/js/сharacters/Dwart.js
+++ src/js/сharacters/Dwart.js
@@ -1,6 +1,23 @@
-import Warrior from './Warrior.js';
-import Axe from '../weapons/Axe.js';
+import { Warrior } from './Warrior.js';
+import { Axe } from '../weapons/Axe.js';
 
-export default class Dwart extends Warrior {
-  static startWeapon = Axe;
+export class Dwart extends Warrior {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 130;
+        this.attack = 15;
+        this.luck = 20;
+        this.description = 'Гном';
+        this.weapon = new Axe();
+        this.hitCount = 0;
+    }
+
+    takeDamage(damage) {
+        this.hitCount++;
+        if (this.hitCount % 6 === 0 && this.getLuck() > 0.5) {
+            super.takeDamage(damage / 2);
+        } else {
+            super.takeDamage(damage);
+        }
+    }
 }
