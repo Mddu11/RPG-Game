b797184d16025088ccb86dcec5bdcd778fa1c465
Mddu11 <vodilov_vano@mail.ru>
2025-12-18 17:54:28 +0300

    Update Crossbowman.js
    Refactor Crossbowman class to include constructor and weapon initialization.

--- src/js/сharacters/Crossbowman.js
+++ src/js/сharacters/Crossbowman.js
@@ -1,6 +1,14 @@
-import Archer from './Archer.js';
-import LongBow from '../weapons/LongBow.js';
+import { Archer } from './Archer.js';
+import { LongBow } from '../weapons/LongBow.js';
 
-export default class Crossbowman extends Archer {
-  static startWeapon = LongBow;
+export class Crossbowman extends Archer {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 85;
+        this.attack = 8;
+        this.agility = 20;
+        this.luck = 15;
+        this.description = 'Арбалетчик';
+        this.weapon = new LongBow();
+    }
 }
